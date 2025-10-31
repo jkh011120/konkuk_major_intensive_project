2df217e7ebf079874d4e1d92c9c6e6c06f5d433e
jkh011120 <mica01120@gmail.com>
2025-10-31 14:53:20 +0900

    add MapScreen & test_code

--- app/src/main/java/com/example/konkuk_major_intensive_project/MainActivity.kt
+++ app/src/main/java/com/example/konkuk_major_intensive_project/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.konkuk_major_intensive_project.screen.MapScreen
 import com.example.konkuk_major_intensive_project.ui.theme.Konkuk_major_intensive_projectTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,18 +16,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Konkuk_major_intensive_projectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MapScreen()
+//            Konkuk_major_intensive_projectTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
         }
     }
 }
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
---
+++ app/src/main/java/com/example/konkuk_major_intensive_project/screen/MapScreen.kt
@@ -0,0 +1,72 @@
+package com.example.konkuk_major_intensive_project.screen
+
+
+import android.Manifest
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.naver.maps.map.compose.ExperimentalNaverMapApi
+import com.naver.maps.map.compose.LocationTrackingMode
+import com.naver.maps.map.compose.MapProperties
+import com.naver.maps.map.compose.MapUiSettings
+import com.naver.maps.map.compose.NaverMap
+import com.naver.maps.map.compose.rememberCameraPositionState
+import com.naver.maps.map.compose.rememberFusedLocationSource
+
+@OptIn(ExperimentalNaverMapApi::class, ExperimentalPermissionsApi::class)
+@Composable
+fun MapScreen(){
+    val context = LocalContext.current
+    val permissionsState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+    )
+    LaunchedEffect(Unit){
+        permissionsState.launchMultiplePermissionRequest()
+    }
+    val granted = permissionsState.permissions.any {it.status.isGranted}
+
+    val cameraPositionState = rememberCameraPositionState()
+    val locationSource = rememberFusedLocationSource()
+
+    NaverMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
+        locationSource = locationSource,
+        properties = MapProperties(
+            locationTrackingMode = if(granted) LocationTrackingMode.Face else LocationTrackingMode.None
+        ),
+        uiSettings = MapUiSettings(
+            isLocationButtonEnabled = true
+        )
+    ){
+
+    }
+
+    if (!granted) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "위치 권한이 필요합니다.",
+                color = Color.Red,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+
+}
